33965dd540972d753bf49c92d1e30bad12675ef2
sominini <115604789+sominini@users.noreply.github.com>
2023-03-08 15:29:13 +0900

    게시판리스트

--- src/main/java/com/aw/anyware/board/model/vo/Board.java
+++ src/main/java/com/aw/anyware/board/model/vo/Board.java
@@ -1,5 +1,28 @@
 package com.aw.anyware.board.model.vo;
 
+import java.sql.Date;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+
+@NoArgsConstructor
+@Setter
+@Getter
+@ToString
 public class Board {
+	
+	private int boardNo;
+	private String boardWriter;
+	private String boardTitle;
+	private String boardContent;
+	private String originName;
+	private String changeName;
+	private Date createDate;
+	private int count;
+	private String status;
+	private int category;
 
 }
