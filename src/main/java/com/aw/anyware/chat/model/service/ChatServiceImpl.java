c55b76c885a74b16d1be30c4a437fe188147ae9f
Munjiring <115603677+Munjiring@users.noreply.github.com>
2023-03-16 17:29:33 +0900

    (임시)add : 프로젝트 인원 관리 jsp 및 model 추가
    (임시)add : 프로젝트 인원 관리 jsp 및 model 추가
    author : 김현지
    
    - 집에서 한 것 푸쉬 안해서 임시로 작성
    - add projectScheduleView.jsp
    - add ChatService, ChatServiceImpl, ChatDao
    - add chat-mapper.xml

---
+++ src/main/java/com/aw/anyware/chat/model/service/ChatServiceImpl.java
@@ -0,0 +1,31 @@
+package com.aw.anyware.chat.model.service;
+
+import java.util.ArrayList;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.aw.anyware.chat.model.dao.ChatDao;
+import com.aw.anyware.member.model.vo.Member;
+
+@Service
+public class ChatServiceImpl implements ChatService {
+	
+	@Autowired
+	private SqlSessionTemplate sqlSession;
+	
+	@Autowired
+	private ChatDao cDao;
+
+	@Override
+	public ArrayList<Member> selectDept() {
+		return cDao.selectDept(sqlSession);
+	}
+
+	@Override
+	public ArrayList<Member> selectMember() {
+		return null;
+	}
+
+}
