474641a3ec557e6c832f66d7cfd28ac56ef40629
Munjiring <115603677+Munjiring@users.noreply.github.com>
2023-03-23 10:16:35 +0900

    chore : config에 type 수정
    chore : config에 type 수정
    author : 김현지
    
    - config.xml에 PjMem 수정

---
+++ src/main/java/com/aw/anyware/project/model/vo/PjMem.java
@@ -0,0 +1,19 @@
+package com.aw.anyware.project.model.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+public class PjMem {
+	
+	private int projectNo;
+	private int memberNo;
+
+}
